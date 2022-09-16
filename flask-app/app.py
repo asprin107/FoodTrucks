2109b233544122f8bc06c491d5d5016a0b0343d8
asprin107 <asprin107@gmail.com>
2022-09-17 05:02:12 +0900

    Add runable test set.

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request, render_template
 import sys
 import requests
 
-es = Elasticsearch(host='localhost')
+es = Elasticsearch(host='es')
 
 app = Flask(__name__)
 
